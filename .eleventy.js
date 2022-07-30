eebf0e18406906ae5c8d718849c3a2fecb9abf4d
Vahe Abovyan <vahe.abov@gmail.com>
2022-07-30 23:14:41 +0400

    add robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const { DateTime } = require('luxon')
+const { DateTime } = require('luxon');
 
 
 module.exports = function (eleventyConfig) {
@@ -6,15 +6,16 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-  })
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
       input: 'src',
       output: 'public'
     }
-  }
-}
+  };
+};
